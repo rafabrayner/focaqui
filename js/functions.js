81996caa3b51094f9f2e5be70c597b9678f69b3a
Rafael <rafabrayner92@gmail.com>
2018-08-19 09:21:40 -0300

    App

---
+++ js/functions.js
@@ -0,0 +1,420 @@
+$(document).ready(function () {
+
+  $("#buttonBusca").click(function () {
+
+      var optionTeste = $("#selectTeste").val();
+
+      if (optionTeste == 0) {
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=16lZwDPYjlxCTSEuGCqSNJdFrhkXZ-3ig&z=13");
+
+
+
+      } else if (optionTeste == 32) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1-aKI-TEWYmKgx-1tALWMHIBvgAJOFUMw");
+
+
+
+      } else if (optionTeste == 17) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1zKCEyEV_dMxFPPQT1ywcq6LaLQfNHlZL");
+
+
+
+      } else if (optionTeste == 19) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1ZMNBrEQQmVb3I3U0ZRS0QrF7MGVgRLhd");
+
+
+
+      } else if (optionTeste == 26) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1rhgjrocvJjpJY-lLuOpSucU7PpPNFFgC");
+
+
+
+      } else if (optionTeste == 48) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1tVKMiZc5aWGtsNtNf3IGKW9iSIV3Jhjo");
+
+
+
+      } else if (optionTeste == 39) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1aaSxXTGwmzxtTSXPqqTC26fFcCvhlkDi");
+
+
+
+      } else if (optionTeste == 14) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1yaA3zx1itq2Fho17xzv4fzEy6vKVtXyy");
+
+
+
+      } else if (optionTeste == 8) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1fdkq6zsLuEbRqFHZLlfqYouZo15JadXX");
+
+
+
+      } else if (optionTeste == 17) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1F0J738Nw_iSp6x7560Pcg_uYccU6w8jz");
+
+
+
+      } else if (optionTeste == 24) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1vYLbrc2S7iWfcAaW9MZ-rXyN4oiocG0L");
+
+
+
+      } else if (optionTeste == 34) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1LeKkZjKsv5dh5qJb9qL-vY25AmUNopnL");
+
+
+
+      } else if (optionTeste == 13) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1me6bDsJeWdHM_62icVXidAyBwhe7tetl");
+
+
+
+      } else if (optionTeste == 33) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=14bhOJzbZFX9A5AzpEwa0WPk7CTajn33y");
+
+
+
+      } else if (optionTeste == 29) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1M5hv77p1svt0Vu-ErHoimYvBX95Pn6RR");
+
+
+
+      } else if (optionTeste == 40) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1tW4tEmKbfWQWYfndFAswpZDpB1nXjv-B");
+
+
+
+      } else if (optionTeste == 31) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1r5B-wpSDGYq7HMpkhuA5uMOj9kLL226-");
+
+
+
+      } else if (optionTeste == 2) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1gJeqlV0uLgutGDm4Tb5QMcv_gLMtceDL");
+
+
+
+      } else if (optionTeste == 16) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1f5_vcAWqXgvyCbUSqtr0HSJV1YmU88TW");
+
+
+
+      } else if (optionTeste == 27) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1ojMfiQ6HGyK33WK-7SVgxB1AayHare_q");
+
+
+
+      } else if (optionTeste == 46) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1m8_lJXa4H0-w9TK2hSSyfsS8FCfOmDlw");
+
+
+
+      } else if (optionTeste == 22) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1QsvlLXo-RR5ianjo__ezn2wUadqwCsF9");
+
+
+
+      } else if (optionTeste == 11) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1w-ReeRrqSWOGnO5hkSI4-_efO6W-Qqki");
+
+
+
+      } else if (optionTeste == 25) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1DZEFQc9aqz6BTI9mruBjQyTfyYy7Qbbk");
+
+
+
+      } else if (optionTeste == 41) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1lzU8kRvGUBhDJInjfKykDfbCb0MTczkM");
+
+
+
+      } else if (optionTeste == 23) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1OZBEe2kfS6c06WqVZxrUZJTQa56SfGpb");
+
+
+
+      } else if (optionTeste == 7) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1jVNgbejPzS0BuG3h38PwoOXxDM2WOa3-");
+
+
+
+      } else if (optionTeste == 15) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=13ESedMFI3E1LJc8cNyLF7UJwM9nhrSPL");
+
+
+
+      } else if (optionTeste == 44) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1N9WzK8bez0kjM8KKyalWvjuhUmUN-JLX");
+
+
+
+      } else if (optionTeste == 38) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1JJpZuHmyeL2XL6oa1fDWcqZA2aR9UT9C");
+
+
+
+      } else if (optionTeste == 43) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1L3gxXuQ1OFl13h8QVyHleqoW-HQW8hCa");
+
+
+
+      } else if (optionTeste == 30) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1Cyk1ULRLP8cj56yvH0GFYEvQLamFOZP6");
+
+
+
+      } else if (optionTeste == 28) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1Ex185g5LDkqUmp67gX-N6JO9FaBv_Axk");
+
+
+
+      } else if (optionTeste == 49) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1773-HG4AwLkRQWFYqXdpxgigi2cLlslT");
+
+
+
+      } else if (optionTeste == 35) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1773-HG4AwLkRQWFYqXdpxgigi2cLlslT");
+
+
+
+      } else if (optionTeste == 6) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1VraadEe739vNZEEuuemKDhN_ZfnRpvWG");
+
+
+
+      } else if (optionTeste == 47) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=11lx_KZpjZAXCFQW1R60UQRGhRsS9P1eL");
+
+
+
+      } else if (optionTeste == 50) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=13-ndmuTzaNzrOuvZm-ZJklKxBYQt62X2");
+
+
+
+      } else if (optionTeste == 42) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1FLu9-E2-ygUu3WPWY_zc8yiqfIe9qD9w");
+
+
+
+      } else if (optionTeste == 21) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1V2zuAJ2i1j4JMe0kV_NaCb5F7EpZrU0R");
+
+
+
+      } else if (optionTeste == 1) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1kaf2TsVKQVBoKrXWuOJ8xFDgMs4fA3m8");
+
+
+
+      } else if (optionTeste == 18) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1nUIuAHTKf1f3Gg3CKH7RA0hZJuKipPVS");
+
+
+
+      } else if (optionTeste == 12) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1vWuiUVIHjdVd63dfyeF8gqAq3SMkAJwL");
+
+
+
+      } else if (optionTeste == 3) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1doX8iBbcrnuZSdHEZpECw7PYJPEXMmFL");
+
+
+
+      } else if (optionTeste == 10) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1ibhtBhmZoZZJMsuEoOsn7912Yd_EJmkB");
+
+
+
+      } else if (optionTeste == 20) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1jTdxi7kisLVxcQa8u6eAjasjaVn3oqo0");
+
+
+
+      } else if (optionTeste == 4) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1M9stFMFiaZygfs5s6iQmHI_3xtrsUb8d");
+
+
+
+      } else if (optionTeste == 5) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1DN2xCxaDX7lXQyx0l89ZJAZVmtCIb-aA");
+
+
+
+      } else if (optionTeste == 36) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1xOdd2D6GUz-nhX6aBOSydSmfDd2JbtpO");
+
+
+
+      } else if (optionTeste == 9) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1YrtXtSok9rtfBkBnGRWnhQdOaLKDq-mJ");
+
+
+
+      } else if (optionTeste == 45) {
+
+
+
+          $("#iframeTeste").attr("src", "https://www.google.com/maps/d/embed?mid=1ytT7In4VUjeuwMOluFYHKOIU_mjhGkO3");
+
+
+
+      }
+
+
+
+  });
+
+
+
+});
